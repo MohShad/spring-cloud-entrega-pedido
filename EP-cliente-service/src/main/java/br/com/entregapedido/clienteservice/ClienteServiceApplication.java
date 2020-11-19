2ce29ba70f2386a238fc77d09dc22e1e18a535b2
Mohammad Shadnik <c0618952@vale.com>
2020-11-19 15:10:14 -0300

    Securing Eureka Dicovery server

--- EP-cliente-service/src/main/java/br/com/entregapedido/clienteservice/ClienteServiceApplication.java
+++ EP-cliente-service/src/main/java/br/com/entregapedido/clienteservice/ClienteServiceApplication.java
@@ -1,10 +1,12 @@
 package br.com.entregapedido.clienteservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableDiscoveryClient
@@ -13,8 +15,13 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 public class ClienteServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ClienteServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ClienteServiceApplication.class, args);
+    }
+
+    @Bean
+    public HttpTraceRepository htttpTraceRepository() {
+        return new InMemoryHttpTraceRepository();
+    }
 
 }
